0418f8a2ce554a835b3c0633a13bede72f07f4a8
markilox <marcosm2004@gmail.com>
2025-11-29 18:33:06 +0100

    Actualización del del hasheo de password

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -7,12 +7,5 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true }
 }, { timestamps: true });
 
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
-
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
 
 module.exports = mongoose.model("User", userSchema);
