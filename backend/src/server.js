8708abecb036b77df326b81cdc3a40c3ea608814
markilox <marcosm2004@gmail.com>
2025-12-02 12:33:38 +0100

    OpenApi

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 const helmet = require("helmet");
 require('dotenv').config();
 
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+const path = require("path");
+
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 
+const swaggerDocument = yaml.load(path.join(__dirname, "openapi.yaml"));
 connectDB();
 
 const app = express();
@@ -16,11 +21,12 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
-// 🔥 MUY IMPORTANTE: primero procesar formularios y JSON
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// 🟢 Después montar las rutas
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/cart", cartRoutes);
